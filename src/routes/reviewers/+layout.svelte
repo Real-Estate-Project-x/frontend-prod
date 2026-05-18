a61fe661a3b4088d097594bb928004a0976619f3
Abel Ani <abelanico6@gmail.com>
2026-05-18 17:35:04 +0100

    Added update to home-page

---
+++ src/routes/reviewers/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Footer from "$lib/components/shared/Footer.svelte";
+    import AgencyNavbar from "$lib/components/shared/AgencyNavbar.svelte";
+    import "../../app.css";
+    import "./layout.css";
+  
+  let { children } = $props();
+</script>
+  
+<AgencyNavbar />
+  
+  {@render children()}
+  
+<Footer />
+  
